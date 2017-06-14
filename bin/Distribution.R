408897623df9b7482ac4f4da8c2bf27539faebbe
itachi125 <tugianghaidang125@gmail.com>
2017-06-14 09:50:17 +0700

    Update Code _btnCancel and fix

--- bin/Distribution.R
+++ bin/Distribution.R
@@ -1,6 +1,6 @@
 # Create a sequence of numbers between 1 and 300 incrementing by 1.
 	x <- seq(1, 300, by = 1)
 	# Choose the mean as 2.5 and standard deviation as 0.5.
-	y <- dnorm(x, mean = 185.8286757153865, sd = 24.36694517022629)
+	y <- dnorm(x, mean = 117.19343494725724, sd = 9.469109611318789)
 	print(y)
-	write.csv(y,"1_output.csv", row.names = FALSE)
+	write.csv(y,"Result\\TempR\\1_output.csv", row.names = FALSE)
