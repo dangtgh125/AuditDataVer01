216b9c0de46d7cb683884a104f8b4901c55141bb
itachi125 <tugianghaidang125@gmail.com>
2017-07-06 12:53:52 +0700

    Update code Verify pick random file in one block
    Còn lỗi chênh lệch giá trị giữa 2 vấn đế của luận văn còn khá lớn, cần
    xem lại công thức tính

--- src/Distribution.R
+++ src/Distribution.R
@@ -1,6 +1,6 @@
 # Create a sequence of numbers between 1 and 40 incrementing by 1.
 	x <- seq(1, 40, by = 1)
 	# Choose the mean as 2.5 and standard deviation as 0.5.
-	y <- dnorm(x, mean = 29.19004924493706, sd = 3.921840481142067)
+	y <- dnorm(x, mean = 31.665595769470986, sd = 7.746759570018766)
 	print(y)
 	write.csv(y,"Result\\TempR\\2_output.csv", row.names = FALSE)
